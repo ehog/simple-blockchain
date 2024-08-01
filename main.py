33381b572b5588ae680461aa91f4e330b979f6ca
ehog <edgar@edgarortiz.com>
2024-08-01 13:49:30 -0400

    Changes to be committed: new file:   main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+# Python program to create Blockchain
+ 
+# For timestamp
+import datetime
+ 
+# Calculating the hash
+# in order to add digital
+# fingerprints to the blocks
+import hashlib
+ 
+# To store data
+# in our blockchain
+import json
+ 
+# Flask is for creating the web
+# app and jsonify is for
+# displaying the blockchain
+from flask import Flask, jsonify
+
+class Blockchain:
+    def __init__(self) -> None:
+        pass
+    
